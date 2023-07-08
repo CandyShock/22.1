89dedf0b36e1f0e0eca8f3726608ff7dfee3d50f
CandyShock <mrhell699@gmail.com>
2023-07-08 17:43:32 +0300

    test com

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nomination', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('nomination', 'description')
+
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nomination')
+    list_filter = ('nomination',)
+    search_fields = ('nomination', 'description')
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    nomination = models.CharField(max_length=30, verbose_name='наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='картинка', null=True, blank=True)
+    category = models.CharField(max_length=30)
+    price = models.IntegerField(verbose_name='цена')
+    date = models.DateTimeField(verbose_name='дата')
+    last_modified_date = models.DateTimeField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f"{self.nomination}\n{self.description}\n{self.category}\n{self.price}\n{self.date}\n" \
+               f"{self.last_modified_date}\n"
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+    nomination = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return f"{self.nomination}\n{self.description}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
